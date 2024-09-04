736ed7596f03d3bbf4c05f39122cb282e19c4249
BishalParajuli2057 <157657392+BishalParajuli2057@users.noreply.github.com>
2024-09-04 18:18:26 +0300

    o

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ document.getElementById('my-button').addEventListener('click', function() {
     
 });
 
+document.getElementById('add-data').addEventListener('click', function() {
+    const ul = document.getElementById('my-list');
+    const li = document.createElement('li');
+    li.textContent = 'New list item'; 
+    ul.appendChild(li);
+});
+
 
 const textArea = document.getElementById('custom-text').value;
 
