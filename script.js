0fd7609d01e30e5bfa07eac487dd4ab8616491f8
BishalParajuli2057 <157657392+BishalParajuli2057@users.noreply.github.com>
2024-09-04 17:55:16 +0300

    gg

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById('my-button').addEventListener('click', function() {
     document.querySelector('h1').textContent = 'Moi maailma';
     
 });
+
+
+document.getElementById('add-data').addEventListener('click', function() {
+    const ul = document.getElementById('my-list');
+    const li = document.createElement('li');
+    li.textContent = 'New list item'; 
+    ul.appendChild(li);
+});
